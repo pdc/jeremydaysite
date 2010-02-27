c69a93348a176dac3bba1b5806c390edc030fa64
pdc <pdc@alleged.org.uk>
2010-02-27 22:18:11 +0000

    Make the addition of the LiveJourtnal section faster.
    Before I had the Twitter download added as a SCRIPT tag with
    the SRC referring to Twitters URL. this meant that the document ready
    event did not fire until it had been returned. Changed to use
    jQuery's Ajax mechanism, which automatically switches to the
    JSONP approach when used cross-site. This means the two sections are
    downloaded in parallel.

--- static/js/front.js
+++ static/js/front.js
@@ -1,25 +1,20 @@
 /* Jazz up the index page smidgen */
 
-// We are loading the Twitter feed, so mark the heading accordingly.
-$('#cleanskies').prev('h2').addClass('loading');
 
 /**
 * Process a bunch of Twitter tweets.
 *
 * This is called via a SCRIPT tag at the bottom of the host page.
 */
-function twitter(data) {
-   $(document).ready(function () {
-    var list = $('#cleanskies');
-    $(list).prev('h2').removeClass('loading');
+function twitterTo(data, list) {
     for (var i = 0; i < data.results.length; ++i) {
         var tweet = data.results[i];
         var link = $('<a>').attr('href', 'http://twitter.com/cleanskies/statuses/' + tweet.id);
-        
+    
         // Can we use the image without making a mess?
         var src = 'http://urlimg.com/square/16/' + tweet.profile_image_url.slice(7);
         $('<img>').attr({src: src, alt: ''}).appendTo(link);
-        
+    
         // The text uses HTML escapes for ampersands etc.,
         // so I could just use jQueryâ€™s html method here,
         // but do I want to trust Twitter to be immune to XSS attacks?
@@ -27,7 +22,7 @@ function twitter(data) {
         text = tweet.text.replace('&lt', '<').replace(/&amp;/, '&');
         $('<span>').text(text).appendTo(link);
         link.append(' ');
-        
+    
         // Convert the date of the tweet to a relative timestamp.
         var created = Date.parse(tweet.created_at);
         var nowish = new Date().getTime();
@@ -47,27 +42,39 @@ function twitter(data) {
           ago = 'just now';
         }
         $('<small>').attr('class', 'permalink').append($('<b>').text(ago)).appendTo(link);
-        
+    
         // Add the link as a list item.
         var item = $('<li>').attr('class', 'big');
         item.append(link);
         list.append(item);
     }   
- });
 }
 
 $(document).ready(function () {
-    $('#livejournal')
-        .prev('h2').addClass('loading');
+    var livejournal$ = $('#livejournal');
+    livejournal$.prev('h2').addClass('loading');
     $.ajax({
         url: 'livejournal',
         dataType: 'json',
         success: function (data, textStatus, request) {
             if (data.success) {
-                $('#livejournal')
-                    .html(data.body)
-                    .prev('h2').removeClass('loading');
+                livejournal$.html(data.body);
             }
+        },
+        complete: function (request, status) {
+            livejournal$.prev('h2').removeClass('loading');
         }
+    });
+    
+    // We are loading the Twitter feed, so mark the heading accordingly.
+    var twitter$ = $('#cleanskies');
+    twitter$.prev('h2').addClass('loading');
+    $.ajax({
+        url: 'http://search.twitter.com/search.json?q=from%3Acleanskies',
+        dataType: 'jsonp',
+        success: function (data, textStatus, request) {
+            twitterTo(data, twitter$);
+            twitter$.prev('h2').removeClass('loading');
+        },
     });
 });
