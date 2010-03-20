168a9de8d150b4a05f6f6dad4bb10a5dbdcbc76b
pdc <pdc@alleged.org.uk>
2010-03-20 16:42:17 +0000

    Made the JavaScript for checking image width more robust, I think.

--- static/js/strip.js
+++ static/js/strip.js
@@ -1,11 +1,22 @@
 
 $(document).ready(function () {
-    if ($('#strip img').width() > 800) {
-        $('#strip-nav')
-            .hover(
-                function () { $('h2', this).next().slideDown(); },
-                function () { $('h2', this).next().slideUp(); }
-            )
-            .find('h2', this).next().slideUp();
+    var isImageWidthChecked = false;
+    var img$ = $('#strip img');
+    var checkImageWidth = function (img$) {
+        if (!isImageWidthChecked) {
+            if (img$.width() > 800) {
+                $('#strip-nav')
+                    .hover(
+                        function () { $('h2', this).next().slideDown(); },
+                        function () { $('h2', this).next().slideUp(); }
+                    )
+                    .find('h2', this).next().slideUp();
+            }
+            isImageWidthChecked = true;
+        }
     }
+    if (img$.get().complete) {
+        checkImageWidth(img$);
+    }
+    img$.load(checkImageWidth(img$));
 });
