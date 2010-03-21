a70e96ff8b52c935066a739652e0e887b7715e91
pdc <pdc@alleged.org.uk>
2010-03-21 10:57:04 +0000

    OK, this time for sure: more reliable image-width-checking code.

--- static/js/strip.js
+++ static/js/strip.js
@@ -1,24 +1,30 @@
 
-$(document).ready(function () {
-    var isHoverAdded = false;
-    var img$ = $('#strip img');
-    var checkImageWidth = function (img$) {
-        if (!isHoverAdded && img$.width() > 800) {
-             $('#strip-nav')
-                .hover(
-                    function () { 
-                        $('h2', this).next().slideDown(); 
-                    },
-                    function () {
-                        $('h2', this).next().slideUp(); 
-                    }
-                )
-                .find('h2', this).next().slideUp();
-            isHoverAdded = true;
-        }
+
+function checkImageWidth(img$) {
+    if (img$.width() > 800) {
+         $('#strip-nav:not(.hover-added)')
+            .hover(
+                function () { 
+                    $('h2', this).next().slideDown(); 
+                },
+                function () {
+                    $('h2', this).next().slideUp(); 
+                }
+            )
+            .find('h2', this).next().slideUp()
+            .addClass('hover-added');
     }
+}
+
+$(document).ready(function () {
+    var img$ = $('#strip img');
     if (img$.get(0).complete) { // This is intended to get it when the image comes from the cache.
         checkImageWidth(img$);
+        img$.addClass('checked-complete');
     }
-    img$.load(checkImageWidth(img$));
+});
+
+$('#strip img').load(function () {
+    checkImageWidth($(this));
+    $(this).addClass('checked-loaded');
 });
