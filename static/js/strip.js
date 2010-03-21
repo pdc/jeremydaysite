6009890e7383d7bc15c52b9ee5b5a12b4a58163f
pdc <pdc@alleged.org.uk>
2010-03-21 10:34:38 +0000

    Another attempt to get the image-width check to run reliably.

--- static/js/strip.js
+++ static/js/strip.js
@@ -1,21 +1,23 @@
 
 $(document).ready(function () {
-    var isImageWidthChecked = false;
+    var isHoverAdded = false;
     var img$ = $('#strip img');
     var checkImageWidth = function (img$) {
-        if (!isImageWidthChecked) {
-            if (img$.width() > 800) {
-                $('#strip-nav')
-                    .hover(
-                        function () { $('h2', this).next().slideDown(); },
-                        function () { $('h2', this).next().slideUp(); }
-                    )
-                    .find('h2', this).next().slideUp();
-            }
-            isImageWidthChecked = true;
+        if (!isHoverAdded && img$.width() > 800) {
+             $('#strip-nav')
+                .hover(
+                    function () { 
+                        $('h2', this).next().slideDown(); 
+                    },
+                    function () {
+                        $('h2', this).next().slideUp(); 
+                    }
+                )
+                .find('h2', this).next().slideUp();
+            isHoverAdded = true;
         }
     }
-    if (img$.get().complete) {
+    if (img$.get(0).complete) { // This is intended to get it when the image comes from the cache.
         checkImageWidth(img$);
     }
     img$.load(checkImageWidth(img$));
