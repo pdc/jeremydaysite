46a0091507eecba27c00cd0b14f28fb766ae47b5
pdc <pdc@alleged.org.uk>
2011-06-19 17:55:04 +0100

    Add setting for HTTPLIB2 cache dir

--- frontpage/views.py
+++ frontpage/views.py
@@ -33,7 +33,7 @@ def front_page(request):
     text_file = os.path.join(settings.FRONTPAGE_DIR,'introduction.md')
     text = codecs.open(text_file, 'r', 'UTF-8').read()
     tws = twslib.get_tws(settings.TWS_FILE, settings.TWS_SRC_PREFIX)
-    
+
     other_sites = twslib.get_sites(os.path.join(settings.FRONTPAGE_DIR, 'other-sites.data'))
     return {
         'text': text,
@@ -45,7 +45,7 @@ LIVEJOURNAL_ATOM_CACHE_KEY = 'livejournal-atom-%s-1' % settings.LIVEJOURNAL_ATOM
 LIVEJOURNAL_HTML_CACHE_KEY = 'livejournal-rendered-%s-' % settings.LIVEJOURNAL_URL
 def livejournal(request):
     html = cache.get(LIVEJOURNAL_HTML_CACHE_KEY)
-    
+
     if not html:
         feed = entries_from_livejournal_url(settings.LIVEJOURNAL_URL)
 
@@ -54,7 +54,7 @@ def livejournal(request):
             body = cache.get(LIVEJOURNAL_ATOM_CACHE_KEY)
             if not body:
                 url = 'http://cleanskies.livejournal.com/data/atom'
-                http = httplib2.Http('/tmp/httplib2')
+                http = httplib2.Http(settings.HTTPLIB2_CACHE_DIR)
                 livejournal_response, body = http.request(url, 'GET', headers={'User-Agent': 'jeremyday.org.uk/1.0 (pdc@alleged.org.uk)'})
                 cache.set(LIVEJOURNAL_ATOM_CACHE_KEY, body)
             feed_elt = ET.XML(body)
@@ -68,17 +68,16 @@ def livejournal(request):
                     elif item_elt.tag == '{http://www.w3.org/2005/Atom}content':
                         entry['content'] = item_elt.text
                 feed.append(entry)
-            
+
         if feed:
             template = loader.get_template('jeremyday/livejournal.div.html')
             context = Context({'feed': feed})
             html = template.render(context)
             cache.set(LIVEJOURNAL_HTML_CACHE_KEY, html)
-    
+
     result = {
         'success': True,
         'body': html,
     }
     response = HttpResponse(json.dumps(result, 'UTF-8'), mimetype='text/javascript')
     return response
-    
