30cfe2bf9d4944920e1739c1096d2bb6c886347a
pdc <pdc@alleged.org.uk>
2012-10-28 18:59:51 +0000

    Update tests

--- jeremyday/frontpage/tests.py
+++ jeremyday/frontpage/tests.py
@@ -18,12 +18,12 @@ class SimpleTest(TestCase):
         """
         self.failUnlessEqual(1 + 1, 2)
 
-        
+
     def test_entries(self):
         print os.getcwd()
-        with open('frontpage/lj-test.html', 'rb') as input:
+        with open('jeremyday/frontpage/lj-test.html', 'rb') as input:
             html = input.read()
-            
+
         entries = entries_from_livejournal_html(html)
         self.assertEqual(14, len(entries)) # 20 entries of which one is friends-locked.
         for i, entry in enumerate(entries):
@@ -34,16 +34,16 @@ class SimpleTest(TestCase):
             self.assertEqual(100, entry['userpic']['width'], 'expected userpic.src for entry #%d' % i)
             self.assert_(entry['content'])
             self.assert_('class="user-icon"' not in entry['content'])
-            
+
         self.assertEqual(datetime(2010, 3, 17, 23, 21, 0), entries[0]['published'])
         self.assertEqual(datetime(2010, 3, 16, 21, 41, 0), entries[1]['published'])
         self.assertEqual(datetime(2010, 3, 16, 0, 1, 0), entries[2]['published'])
         self.assertEqual(datetime(2010, 3, 11, 8, 35, 0), entries[4]['published'])
-        
-        self.assertEqual(8, entries[0]['comment_count'])        
+
+        self.assertEqual(8, entries[0]['comment_count'])
         self.assertEqual(9, entries[1]['comment_count'])
         self.assertEqual(0, entries[5]['comment_count']) # Had to search for a post with no comments...!
-        
+
     def test_date_coverage(self):
         self.assertEqual(True, is_date_covered(datetime(2010, 4, 7), datetime(2010, 4, 7)))
         self.assertEqual(False, is_date_covered(datetime(2010, 4, 7), datetime(2010, 4, 6)))
@@ -52,10 +52,9 @@ class SimpleTest(TestCase):
         self.assertEqual(False, is_date_covered(datetime(2010, 4, 7), datetime(2010, 4, 8, 13, 0, 0)))
         self.assertEqual(True, is_date_covered(datetime(2010, 4, 7), datetime(2010, 4, 6, 13, 0, 0)))
         self.assertEqual(False, is_date_covered(datetime(2010, 4, 7), datetime(2010, 4, 6, 10, 0, 0)))
-        
+
     def test_cass_naked_date(self):
         self.assertEqual(datetime(2010, 4, 7), css_naked_date(2010))
-        
+
     def test_cass_naked_date_default(self):
         self.assertEqual(css_naked_date(datetime.now().year), css_naked_date())
-        
