e097549a1f35a961d301e1592c3dd3e7089ba36c
Damian Cugley <pdc@alleged.org.uk>
2018-08-05 11:40:28 +0100

    Update fabfile.

--- fabfile.py
+++ fabfile.py
@@ -10,18 +10,22 @@ env.virtualenv = env.site_name
 env.settings_subdir = env.site_name
 env.django_apps = ['theweeklystrip', 'spreadlinks', 'frontpage']
 
+
 def update_requirements():
-    local("pip freeze | egrep -v 'Fabric|pycrypto|ssh|ecdsa|paramiko' > requirements.txt")
+    local("pipenv lock -r' > requirements.txt")
+
 
 def test():
     with settings(warn_only=True):
-        ##result = local('./manage.py test {0}'.format(' '.join(env.django_apps)), capture=True)
-        result = local('./manage.py test')
+        ## result = local('./manage.py test {0}'.format(' '.join(env.django_apps)), capture=True)
+        result = local('pipenv run python -Wall ./manage.py test')
     if result.failed and not confirm("Tests failed. Continue anyway?"):
         abort("Aborting at user request.")
 
+
 def push():
     local('git push')
+
 
 def deploy():
     test()
@@ -33,11 +37,8 @@ def deploy():
 
     code_dir = '/home/{0}/Sites/{0}'.format(env.site_name)
     with cd(code_dir):
-        run('git pull')
-        run('cp {0}/settings_production.py {0}/settings.py'.format(env.settings_subdir))
+        run('git pull github master')
 
         with prefix('. /home/{0}/virtualenvs/{1}/bin/activate'.format(env.site_name, env.virtualenv)):
             run('pip install -r requirements.txt')
-            run('./manage.py collectstatic --noinput')
-
-    run('touch /etc/uwsgi/emperor.d/{0}.ini'.format(env.site_name))
+            run('envdir /service/%s/env ./manage.py collectstatic --noinput' % (env.site_name,))
